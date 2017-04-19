e23b86f9f4dc3ef78c008abc91d2da29b32bae70
Roman Kuba <roman.kuba@gmail.com>
2017-04-19 10:58:43 +0200

    Use webpack for building

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import beautify from './beautify'
 
-export default class Revue {
+class Revue {
 
   constructor(component, data={}) {
     this._component = component
@@ -21,12 +21,30 @@ export default class Revue {
     }).$mount(document.createElement('div'));
   }
 
+  _nextTick() {
+    return new Promise((resolve, reject) => {
+      Vue.nextTick(() => resolve())
+    })
+  }
+
   /**
    * HTML
    * -- get a beautified version of the rendered html
    * @return {String}
    */
   get $html () {
+    return new Promise((resolve, reject) => {
+      this._nextTick()
+        .then(() => {
+          return beautify(this._mounted.$el.outerHTML)
+        })
+        .catch((err) => {
+          (reject(err))
+        })
+    })
+
     return beautify(this._mounted.$el.outerHTML)
   }
 }
+
+module.exports = Revue
