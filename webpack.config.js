d4d86f0fcbd502f82075a953eb06ee32433ab17a
Roman Kuba <roman.kuba@gmail.com>
2017-04-19 12:09:32 +0200

    Refine build process

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: "Revue",
+    libraryTarget: "umd"
+  },
+
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
