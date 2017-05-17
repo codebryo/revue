ece185d8bea7cc01fa2031aaf4a989f37076a24d
Roman Kuba <roman.kuba@gmail.com>
2017-05-17 13:38:35 +0200

    Updated Testcomponent Markup

--- spec/component.js
+++ spec/component.js
@@ -4,6 +4,10 @@ export default {
       <h1>Welcome {{ nameProcessed }}</h1>
       <p class="description">{{ desc }}</p>
       <button @click="toggle">Transform Name</button>
+
+      <h2>Share some Love</h2>
+      <span class="love">{{ love }}</span>
+      <button @click="increase">Increase</button>
     </div>
   `,
 
@@ -21,11 +25,17 @@ export default {
       if (this.allcaps) return this.name.toUpperCase();
       return this.name;
     },
+
+    love() {
+    }
   },
 
   methods: {
     toggle() {
       this.allcaps = !this.allcaps;
+    },
+
+    increase() {
     },
   },
 };
